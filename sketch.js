bf13b31c29f0b410de3055cf8f481ec2e09ef874
Sannyuktabiradar2508 <73464108+Sannyuktabiradar2508@users.noreply.github.com>
2021-06-16 18:12:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function preload(){
+
+bucket = loadImage("images/bucket.jpeg");
+chair = loadImage("images/chair.jpg");
+chest = loadImage("images/chest.png");
+coat = loadImage("images/coat.jpeg");
+fan = loadImage("images/fan.jpeg");
+giant1 = loadImage("images/giant1.jpeg");
+giant2 = loadImage("images/giant2.jpeg");
+glove = loadImage("images/glove.png");
+mat = loadImage("images/mat.jpeg");
+monster1 = loadImage("images/monster1.png");
+monster2 = loadImage("images/monster2.png");
+pencil = loadImage("images/pencil.png");
+pillow = loadImage("images/pillow.jpeg");
+pirate1 = loadImage("images/pirate1.jpeg");
+pirate2 = loadImage("images/pirate2.jpeg");
+pirate3 = loadImage("images/pirate3.jpeg");
+sandcastle = loadImage("images/sandcastle.jpeg");
+scarf = loadImage("images/scarf.jpeg");
+starfish = loadImage("images/starfish.jpeg");
+sunglass = loadImage("images/sunglass.png");
+teddybear = loadImage("images/teddybear.png");
+vase = loadImage("images/vase.jpeg");
+
+
+
+}
